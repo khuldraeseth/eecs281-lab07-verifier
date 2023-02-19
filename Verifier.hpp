1db63cffb831b6055f72663f241e56a594187f0c
Khuldraeseth <28711596+khuldraeseth@users.noreply.github.com>
2023-02-18 23:18:24 -0500

    Make Verifier more general

--- Verifier.hpp
+++ Verifier.hpp
@@ -4,17 +4,17 @@
 class Verifier {
 public:
     template <typename Table>
-    static auto numElements(Table&& table) -> auto const& {
+    static auto numElements(Table&& table) -> auto& {
         return table.numElements;
     }
 
     template <typename Table>
-    static auto numDeleted(Table&& table) -> auto const& {
+    static auto numDeleted(Table&& table) -> auto& {
         return table.numDeleted;
     }
 
     template <typename Table>
-    static auto buckets(Table&& table) -> auto const& {
+    static auto buckets(Table&& table) -> auto& {
         return table.buckets;
     }
 };
