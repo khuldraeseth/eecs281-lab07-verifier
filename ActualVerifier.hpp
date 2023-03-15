4326c0c11c0a9aa1fb292063e622aff8aeb4c9db
Khuldraeseth <28711596+khuldraeseth@users.noreply.github.com>
2023-03-15 09:40:57 -0400

    Implement erase and subscript

--- ActualVerifier.hpp
+++ ActualVerifier.hpp
@@ -64,9 +64,9 @@ class ActualVerifier {
 public:
     using Key = Kwarg<K, struct KeyTag>;
     using Value = Kwarg<V, struct ValueTag>;
-    using ExpectedSize = Kwarg<std::size_t, struct ExpectedSizeTag>;
+    using Size = Kwarg<std::size_t, struct ExpectedSizeTag>;
     using ProbeSequence = Kwarg<std::vector<std::size_t>, struct ProbeSequenceTag>;
-    using InsertResult = Kwarg<bool, struct InsertResultTag>;
+    using Success = Kwarg<bool, struct InsertResultTag>;
 
     template <typename... Args>
     auto size(Args&&... args) const -> ActualVerifier const& {
@@ -84,33 +84,23 @@ public:
         return insertImpl(std::make_tuple(std::forward<Args>(args)...));
     }
 
-    auto erase(Key key, ProbeSequence probeSequence) -> ActualVerifier& {
-#pragma message "TODO: implement erase"
-        std::cout << "Erase called" << std::endl;
-        ProbeSequenceVerifier<K, V, H> psv(table, probeSequence);
-        NotifyingKey<K> notifyingKey { psv, key };
-        static_cast<void>(table.erase(notifyingKey));
-        psv.finalize();
-        return *this;
+    template <typename... Args>
+    auto erase(Args&&... args) -> ActualVerifier& {
+        return eraseImpl(std::make_tuple(std::forward<Args>(args)...));
     }
 
-    auto subscript(Key key, ProbeSequence probeSequence) -> ActualVerifier& {
-#pragma message "TODO: implement subscript"
-        std::cout << "Subscript called" << std::endl;
-        ProbeSequenceVerifier<K, V, H> psv(table, probeSequence);
-        NotifyingKey<K> notifyingKey { psv, key };
-        static_cast<void>(table[notifyingKey]);
-        psv.finalize();
-        return *this;
+    template <typename... Args>
+    auto subscript(Args&&... args) -> ActualVerifier& {
+        return subscriptImpl(std::make_tuple(std::forward<Args>(args)...));
     }
 
 private:
     template <typename... Args>
     auto sizeImpl(std::tuple<Args...> args) const -> ActualVerifier const& {
-        static_assert(count<ExpectedSize, Args...> > 0, "Error: size requires an expected size");
-        static_assert(count<ExpectedSize, Args...> < 2, "Error: size allows no more than expected size");
+        static_assert(count<Size, Args...> > 0, "Error: size requires an expected size");
+        static_assert(count<Size, Args...> < 2, "Error: size allows no more than expected size");
 
-        auto const expected = std::get<ExpectedSize>(args).value;
+        auto const expected = std::get<Size>(args).value;
 
         auto const numBuckets = Verifier::buckets(table).size();
         if (expected > numBuckets) {
@@ -142,9 +132,9 @@ private:
 
     template <typename... Args>
     auto insertImpl(ProbeListener<K>& listener, std::tuple<Args...> args) -> ActualVerifier& {
-        if constexpr (count<InsertResult, Args...> != 0) {
-            static_assert(count<InsertResult, Args...> < 2, "Error: insert allows no more than one insert result");
-            auto ec = EqualityChecker<bool> { std::get<InsertResult>(args).value };
+        if constexpr (count<Success, Args...> != 0) {
+            static_assert(count<Success, Args...> < 2, "Error: insert allows no more than one insert result");
+            auto ec = EqualityChecker<bool> { std::get<Success>(args).value };
             return insertImpl(listener, ec, args);
         } else {
             auto nec = NotEqualityChecker<bool> {};
@@ -162,9 +152,88 @@ private:
         auto const key = std::get<Key>(args).value;
         auto const value = std::get<Value>(args).value;
 
-        // NotifyingKey<K> notifyingKey { listener, key };
         auto notifyingKey = NotifyingKey<K> { listener, key };
         auto const result = table.insert(notifyingKey, value);
+        listener.finalize();
+
+        ec(result);
+
+        return *this;
+    }
+
+    template <typename... Args>
+    auto eraseImpl(std::tuple<Args...> args) -> ActualVerifier& {
+        if constexpr (count<ProbeSequence, Args...> != 0) {
+            static_assert(count<ProbeSequence, Args...> < 2, "Error: erase allows no more than one probe sequence");
+            auto psv = ProbeSequenceVerifier<K, V, H>(table, std::get<ProbeSequence>(args).value);
+            return eraseImpl(psv, args);
+        } else {
+            auto npsv = NotProbeSequenceVerifier<K> {};
+            return eraseImpl(npsv, args);
+        }
+    }
+
+    template <typename... Args>
+    auto eraseImpl(ProbeListener<K>& listener, std::tuple<Args...> args) -> ActualVerifier& {
+        if constexpr (count<Success, Args...> != 0) {
+            static_assert(count<Success, Args...> < 2, "Error: erase allows no more than one erase result");
+            auto ec = EqualityChecker<bool> { std::get<Success>(args).value };
+            return eraseImpl(listener, ec, args);
+        } else {
+            auto nec = NotEqualityChecker<bool> {};
+            return eraseImpl(listener, nec, args);
+        }
+    }
+
+    template <typename... Args>
+    auto eraseImpl(ProbeListener<K>& listener, Consumer<bool> const& ec, std::tuple<Args...> args) -> ActualVerifier& {
+        static_assert(count<Key, Args...> > 0, "Error: erase requires a key");
+        static_assert(count<Key, Args...> < 2, "Error: erase allows no more than one key");
+
+        auto const key = std::get<Key>(args).value;
+
+        auto notifyingKey = NotifyingKey<K> { listener, key };
+        auto const result = static_cast<bool>(table.erase(notifyingKey));
+        listener.finalize();
+
+        ec(result);
+
+        return *this;
+    }
+
+    template <typename... Args>
+    auto subscriptImpl(std::tuple<Args...> args) -> ActualVerifier& {
+        if constexpr (count<ProbeSequence, Args...> != 0) {
+            static_assert(count<ProbeSequence, Args...> < 2, "Error: subscript allows no more than one probe sequence");
+            auto psv = ProbeSequenceVerifier<K, V, H>(table, std::get<ProbeSequence>(args).value);
+            return subscriptImpl(psv, args);
+        } else {
+            auto npsv = NotProbeSequenceVerifier<K> {};
+            return subscriptImpl(npsv, args);
+        }
+    }
+
+    template <typename... Args>
+    auto subscriptImpl(ProbeListener<K>& listener, std::tuple<Args...> args) -> ActualVerifier& {
+        if constexpr (count<Value, Args...> != 0) {
+            static_assert(count<Value, Args...> < 2, "Error: subscript allows no more than one subscript result");
+            auto ec = EqualityChecker<V> { std::get<Value>(args).value };
+            return subscriptImpl(listener, ec, args);
+        } else {
+            auto nec = NotEqualityChecker<V> {};
+            return subscriptImpl(listener, nec, args);
+        }
+    }
+
+    template <typename... Args>
+    auto subscriptImpl(ProbeListener<K>& listener, Consumer<V> const& ec, std::tuple<Args...> args) -> ActualVerifier& {
+        static_assert(count<Key, Args...> > 0, "Error: subscript requires a key");
+        static_assert(count<Key, Args...> < 2, "Error: subscript allows no more than one key");
+
+        auto const key = std::get<Key>(args).value;
+
+        auto notifyingKey = NotifyingKey<K> { listener, key };
+        auto const result = table[notifyingKey];
         listener.finalize();
 
         ec(result);
